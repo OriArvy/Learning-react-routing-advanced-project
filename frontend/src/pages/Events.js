cb40d01004fcfcaa3b78d6ae7b660064fa80e526
Rokas <rokas.vaitkevicius1@gmail.com>
2024-01-30 08:02:07 +0200

    Added some fallback errors

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,10 +3,11 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
   const data = useLoaderData();
+  const events = data.events
 
   return (
     <>
-      <EventsList events={data} />
+      <EventsList events={events} />
     </>
   );
 }
@@ -17,9 +18,8 @@ export async function loader() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    // ...
+    throw new Response(JSON.stringify({message: "Could not fetch events"}), {status: 500})
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response
   }
 }
