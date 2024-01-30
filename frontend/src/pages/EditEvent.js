9cd576563efaef609205e490434dbd593f9771b9
Rokas <rokas.vaitkevicius1@gmail.com>
2024-01-31 00:56:12 +0200

    Added edit method, with data access from higher level loader

--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -1,7 +1,13 @@
+import { useRouteLoaderData } from 'react-router-dom'
+import EventForm from '../components/EventForm'
+
 function EditEventPage() {
+  const data = useRouteLoaderData('event-detail')
+  const event = data.event
+
   return (
     <>
-      <h1>Welcome to Edit Event Page</h1>
+      <EventForm event={event}/>
     </>
   )
 }
