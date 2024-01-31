766b220aa89c5742280867a332f9210abe90ef46
Rokas <rokas.vaitkevicius1@gmail.com>
2024-01-31 09:55:37 +0200

    Added newsletter signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,18 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import HomePage from './pages/Home';
-import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction, } from './pages/EventDetail';
-import NewEventPage from './pages/NewEvent';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import EditEventPage from './pages/EditEvent';
-import RootLayout from './pages/Root';
-import EventsRootLayout from './pages/EventsRoot';
 import ErrorPage from './pages/Error';
-import { action as manipulateEventAction } from './components/EventForm'
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from './pages/EventDetail';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
+import EventsRootLayout from './pages/EventsRoot';
+import HomePage from './pages/Home';
+import NewEventPage from './pages/NewEvent';
+import RootLayout from './pages/Root';
+import { action as manipulateEventAction } from './components/EventForm';
+import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
 
 const router = createBrowserRouter([
   {
@@ -16,22 +21,50 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'events', element: <EventsRootLayout />, children: [
-        { index: true, element: <EventsPage />, loader: eventsLoader },
-        { path: ':eventId', id: 'event-detail', loader: eventDetailLoader, children: [
-          { index: true, element: <EventDetailPage />, action: deleteEventAction, },
-          { path: 'edit', element: <EditEventPage />, action: manipulateEventAction }
-        ] },
-        { path: 'new', element: <NewEventPage />, action: manipulateEventAction },
-      ]}
-    ]
-  }
-])
+      {
+        path: 'events',
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ':eventId',
+            id: 'event-detail',
+            loader: eventDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                action: deleteEventAction,
+              },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
+            ],
+          },
+          {
+            path: 'new',
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
+        ],
+      },
+      {
+        path: 'newsletter',
+        element: <NewsletterPage />,
+        action: newsletterAction,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div>
-    <RouterProvider router={router}/>
-  </div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
